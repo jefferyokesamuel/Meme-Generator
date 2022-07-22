ef8829e473c038ef353beb8863143d8b759428ac
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 03:50:00 +0100

    changes

--- meme/src/Memedata.js
+++ meme/src/Memedata.js
@@ -1,5 +1,5 @@
 import React from 'react';
-export default {
+export default{
     "success":true,
     "data": {
         "memes": [
@@ -70,7 +70,63 @@ export default {
                 "height":720,
                 "box_count":4
             },
-            
+            {
+                "id":"222403160",
+                "name":"Bernie I Am Once Again Asking For Your Support",
+                "url":"https:\/\/i.imgflip.com\/3oevdk.jpg",
+                "width":750,
+                "height":750,
+                "box_count":2
+                },
+            {
+                "id":"131940431",
+                "name":"Gru's Plan",
+                "url":"https:\/\/i.imgflip.com\/26jxvz.jpg",
+                "width":700,
+                "height":449,
+                "box_count":4
+            },
+            {
+                "id":"4087833",
+                "name":"Waiting Skeleton",
+                "url":"https:\/\/i.imgflip.com\/2fm6x.jpg",
+                "width":298,
+                "height":403,
+                "box_count":2
+            },
+            {
+                "id":"135256802",
+                "name":"Epic Handshake",
+                "url":"https:\/\/i.imgflip.com\/28j0te.jpg",
+                "width":900,
+                "height":645,
+                "box_count":3
+            },
+            {
+                "id":"80707627",
+                "name":"Sad Pablo Escobar",
+                "url":"https:\/\/i.imgflip.com\/1c1uej.jpg",
+                "width":720,
+                "height":709,
+                "box_count":3
+            },
+            {
+                "id":"438680",
+                "name":"Batman Slapping Robin",
+                "url":"https:\/\/i.imgflip.com\/9ehk.jpg",
+                "width":400,
+                "height":387,
+                "box_count":2
+            },
+            {
+                "id":"93895088",
+                "name":"Expanding Brain",
+                "url":"https:\/\/i.imgflip.com\/1jwhww.jpg",
+                "width":857,
+                "height":1202,
+                "box_count":4
+            },
+                
             ]
         }
     }
